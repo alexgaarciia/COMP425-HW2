38998e0148e0dd64fd0a0e5c12ea104f7177b67d
alexgaarciia <al.garcia636@gmail.com>
2024-02-22 16:46:53 -0500

    finished part 1

--- hough/hough.py
+++ hough/hough.py
@@ -1,9 +1,11 @@
-# import other necessary libaries
+# import other necessary libraries
+import numpy as np
 import matplotlib.pyplot as plt
 from skimage.io import imread
 from skimage.feature import canny
 from skimage.color import rgb2gray
 from utils import create_line, create_mask
+from skimage.transform import hough_line, hough_line_peaks
 
 
 # load the input image and convert to grayscale
@@ -37,11 +39,40 @@ plt.title('Edges in ROI')
 plt.show()
 
 # perform Hough transform
+hough_space, angles, distances = hough_line(masked_edges)
 
 # find the right lane by finding the peak in hough space
+accum, angles_peaks, dists_peaks = hough_line_peaks(hough_space, angles, distances)
+rho, theta = dists_peaks[0], angles_peaks[0]
+xs, ys = create_line(rho, theta, im)
 
 # zero out the values in accumulator around the neighborhood of the peak
+nms_radius = 100
+
+# Convert peak coordinates to accumulator array indices
+peak_index = (np.argmin(np.abs(distances - rho)), np.argmin(np.abs(angles - theta)))
+
+# Apply NMS
+for r_offset in range(-nms_radius, nms_radius + 1):
+    for theta_offset in range(-nms_radius, nms_radius + 1):
+        if 0 <= peak_index[0] + r_offset < hough_space.shape[0] and 0 <= peak_index[1] + theta_offset < hough_space.shape[1]:
+            hough_space[peak_index[0] + r_offset, peak_index[1] + theta_offset] = 0
 
 # find the left lane by finding the peak in hough space
+accum, angles_peaks, dists_peaks = hough_line_peaks(hough_space, angles, distances)
+rho_orange, theta_orange = dists_peaks[1], angles_peaks[1]
+xs_orange, ys_orange = create_line(rho_orange, theta_orange, im)
 
 # plot the results
+# Plotting the original image
+plt.figure(figsize=(10, 7))
+plt.imshow(image_gray, cmap='gray')
+plt.axis('off')
+plt.title('Detected Lanes')
+
+# Plot the first lane (Blue lane)
+plt.plot(xs, ys, '-b', label='Blue Lane')
+
+# Plot the second lane (Orange lane)
+plt.plot(xs_orange, ys_orange, color='orange', label='Orange Lane')
+plt.show()
